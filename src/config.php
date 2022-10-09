eed81a68a576188ef8663c848c492ffe156564ec
astridYeseniaGonzalez <yeseniagonzalez1997@hotmail.es>
2022-10-08 22:33:32 -0600

    first commit

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db = [
+    'host' => 'glasmmy.com',
+    'username' => 'marcocid2',
+    'password' => 'marco123',
+    'db' => 'QUINIELA_PROYECTO' //Cambiar al nombre de tu base de datos
+];
+
+?>
